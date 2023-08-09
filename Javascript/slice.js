474fbdfb7047f3db984617237158baa37d573c46
Muchiri-cmd <107479427+Muchiri-cmd@users.noreply.github.com>
2023-08-09 11:58:58 +0300

    Add files via upload

---
+++ Javascript/slice.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+    // Only change code below this line
+    const new_arr=arr.slice(2,4);
+    return new_arr;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
