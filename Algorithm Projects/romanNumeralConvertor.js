7a53feb99146d61591457c5c9f83ab2ace895b2c
Muchiri-cmd <107479427+Muchiri-cmd@users.noreply.github.com>
2023-08-28 08:16:57 +0300

    Add files via upload

---
+++ Algorithm Projects/romanNumeralConvertor.js
@@ -0,0 +1,29 @@
+function convertToRoman(num) {
+    const romanNumerals= {
+      M:1000,
+      CM:900,
+      D:500,
+      CD:400,
+      C:100,
+      XC:90,
+      L:50,
+      XL:40,
+      X:10,
+      IX:9,
+      V:5,
+      IV:4,
+      I:1
+    }
+    let string='';
+    for (let key in romanNumerals){
+      while(num>=romanNumerals[key]){
+        string+=key;//add roman numerals to string
+        num-=romanNumerals[key];//subtract converted number
+  
+      }
+    }
+  
+   return string;
+  }
+  
+  convertToRoman(36);
